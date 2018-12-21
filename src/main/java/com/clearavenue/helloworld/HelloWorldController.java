e9401b581841c9e2cd96c760c45ba80b679b78fd
Shane Hogan <shogan94@gmail.com>
2018-12-21 12:25:47 -0500

    Hellow world boot app

---
+++ src/main/java/com/clearavenue/helloworld/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.clearavenue.helloworld;
+
+import lombok.extern.java.Log;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log
+public class HelloWorldController {
+
+    @GetMapping("/version")
+    public String getVersion(){
+        log.info("HelloWorld Getting Version");
+        return "1.0.0";
+    }
+
+}
